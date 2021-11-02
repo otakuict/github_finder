7ef446e2dc5952c6d9ecac0dd460849d24d9a08a
gptcoding <otaku.ict@gmail.com>
2021-11-02 16:36:27 +0700

    1102

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/layout/Navbar';
+import UserItem from './components/users/UserItem';
+import Users from './components/users/Users';
 
 function App() {
+ 
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar  />
+      <div className="container">
+      <Users />
+
+      </div>
+     
     </div>
   );
 }
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,13 +1,24 @@
-import React, { Component } from 'react'
+import React  from 'react'
+import PropTypes from 'prop-types'
 
-export class Navbar extends Component {
-    render() {
+ const Navbar =({icon,title})=> {
+    
+  
         return (
             <nav className="navbar bg-primary">
-                <h1>Navbar</h1>
+                <h1>
+                   <i className={icon}/>{title}
+                    </h1>
             </nav>
         )
-    }
+    
 }
-
+Navbar.defaultProps ={
+    title:'Github Finder',
+    icon:'fab fa-github'
+}
+Navbar.propTypes ={
+    title:PropTypes.string.isRequired,
+    icon:PropTypes.string.isRequired
+}
 export default Navbar
