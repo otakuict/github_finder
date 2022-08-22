c7511f19a704014240ed1364996d2186a4072f11
MUM <otaku.ict@gmail.com>
2022-08-22 17:40:56 +0700

    add search and alert

--- src/App.js
+++ src/App.js
@@ -2,32 +2,52 @@
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
 import { Component } from 'react/cjs/react.production.min';
+import PropTypes from  'prop-types' 
 import axios from 'axios';
-
+import  Alert from './components/layout/Alert'
 
 
 class App extends Component {
 
 state = {
   users:[],
-  loading:false
+  loading:false,
+  alert:null
 }
 
-async componentDidMount(){
-  //console.log(process.env.REACT_APP_GITHUB_CLIENT_ID)
-  this.setState({loading:true})
-
-  const res = await  axios.get(`https://api.github.com/users?client_id=
+static propTypes ={
+  searchUsers:PropTypes.func.isRequired
+}
+//search
+  searchUsers = async text =>{
+    this.setState({loading:true})
+    const res = await  axios.get(`https://api.github.com/search/users?q=${text}&lient_id=
   ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
   ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
-  this.setState({users:res.data, loading:false})
+  this.setState({users:res.data.items, loading:false})
+  // console.log(res.data.items)
+  // console.log(res.data)
+  }
+
+  //set Alet
+  setAlert = (msg,type) => {
+    this.setState({alert:{msg, type}})
+    setTimeout(() => {
+      this.setState({alert: null})
+    }, 5000);
   }
  render(){
   return ( 
     <div className="App">
       <Navbar  />
       <div className="container">
+        <Alert alert={this.state.alert} />
+        <Search 
+        searchUsers={this.searchUsers}
+        setAlert={this.setAlert}
+         />
       <Users loading={this.state.loading} users={this.state.users} />
 
       </div>
---
+++ src/components/users/Search.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import PropTypes from  'prop-types' 
+export class Search extends Component {
+    state ={
+        text:''
+    };
+
+    static propTypes = {
+        searchUsers:PropTypes.func.isRequired,
+        setAlert:PropTypes.func.isRequired
+    }
+
+    onChange = e => this.setState({[e.target.name]:e.target.value})
+
+    onSubmit = e => {
+        e.preventDefault();
+        if(this.state.text === ''){
+            this.props.setAlert('please enter Something','light')
+        }else {
+            this.props.searchUsers(this.state.text)
+            this.setState({text:''});
+        }
+      
+    }
+
+  render() {
+    return (
+      <div>
+          <form onSubmit={this.onSubmit} className="form">
+       
+        <input 
+        type="text" 
+        name="text" 
+        placeholder='Search User...'
+        value={this.state.text}
+        onChange={this.onChange}
+        />
+        <input type="submit"  value="Search" className='btn btn-dark btn-block'/>
+
+          </form>
+
+      </div>
+    )
+  }
+}
+
+export default Search
